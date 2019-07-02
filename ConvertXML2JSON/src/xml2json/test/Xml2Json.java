758b2e245c383e226d86ba68d3fbc6aff9dee8ff
James Olin Painter <31042749+JamesOlinPainter@users.noreply.github.com>
2019-07-01 23:28:33 -0400

    Online connection working
    Online connection and most test URLs working.
    The program is retrieving the correct timestamp for most recent RSS update.

--- ConvertXML2JSON/src/xml2json/test/Xml2Json.java
+++ ConvertXML2JSON/src/xml2json/test/Xml2Json.java
@@ -1,9 +1,10 @@
 package xml2json.test;
 
 import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 import org.apache.commons.lang3.StringUtils;
 
 public class Xml2Json {
@@ -28,6 +29,7 @@ public class Xml2Json {
 				  									  "https://newyork.craigslist.org/res/index.rss",
 				  									  "https://losangeles.craigslist.org/tfr/index.rss",
 				  									  "https://washingtondc.craigslist.org/gfs/index.rss"});
+		CompanyAndRssFeedURL.put("Diane Rehm Show", new String[] {"https://dianerehm.org/rss/npr/dr_podcast.xml"});
 		CompanyAndRssFeedURL.put("ESPN", new String[] {"https://www.espn.com/espn/rss/news",
 				  									   "https://www.espn.com/espn/rss/nfl/news",
 				  									   "https://www.espn.com/espn/rss/nba/news",
@@ -56,34 +58,60 @@ public class Xml2Json {
 				  									   "https://www.espn.com/espn/rss/recruiting/usc/news",
 				  									   "https://www.espn.com/espn/rss/recruiting/michigan/news",
 				  									   "https://www.espn.com/espn/rss/index"});
-		  
-		String xmlString = "";
-		xmlString += loadFile("newTest.xml");
-		System.out.println("lastBuildDate : " + StringUtils.substringBetween(xmlString, "<lastBuildDate>", "</lastBuildDate>"));
-		System.out.println("syn:updateBase : " + StringUtils.substringBetween(xmlString, "<syn:updateBase>", "</syn:updateBase>"));
-		System.out.println("pubDate : " + StringUtils.substringBetween(xmlString, "<pubDate>", "</pubDate>"));
-		for(String key : CompanyAndRssFeedURL.keySet()) {
-			System.out.println("Company : " + key);
-			String[] values = CompanyAndRssFeedURL.get(key); 
-			int count = 1;
-			for (String value : values) {
+		
+		URL url;
+	    InputStream inStream = null;
+	    BufferedReader br;
+	    String line, page = "";
+	    
+	    
+	    for(String key : CompanyAndRssFeedURL.keySet()) {
+	    	System.out.println("Company : " + key);
+	    	String[] values = CompanyAndRssFeedURL.get(key);
+	    	int count = 1;
+	    	for (String value : values) {
 				System.out.println("	RSS feed URL #" + count + " : " + value);
 				count++;
-			}
-		}
-	}
-	
-	public static String loadFile(String filename) throws FileNotFoundException {
-		
-		File file = new File(filename);
-	    Scanner inputFile = new Scanner(file);
-	    
-		String fileContents = "";
+				try {
+					url = new URL(value);
+					inStream = url.openStream();  // throws an IOException
+					br = new BufferedReader(new InputStreamReader(inStream));
 
-		while (inputFile.hasNext()) {
-			fileContents += inputFile.nextLine().trim();
-		}
-		inputFile.close();
-		return fileContents;
-	}
-}
+					while ((line = br.readLine()) != null) {
+						page += line;
+					}
+					String mostRecentUpdate, lbd, sub, pd = null;
+					lbd = StringUtils.substringBetween(page, "<lastBuildDate>", "</lastBuildDate>");
+					sub = StringUtils.substringBetween(page, "<syn:updateBase>", "</syn:updateBase>");
+					pd = StringUtils.substringBetween(page, "<pubDate>", "</pubDate>");
+					if (lbd == null) {
+						if (sub == null) {
+							if (pd == null) {
+								mostRecentUpdate = "No Update information";
+							} else {
+								mostRecentUpdate = pd;
+							}
+						} else {
+							mostRecentUpdate = sub;
+						}
+					} else {
+						mostRecentUpdate = lbd;
+					}
+					System.out.println("Most recent update to RSS feed : " + mostRecentUpdate);
+				} catch (MalformedURLException mue) {
+					mue.printStackTrace();
+				} catch (IOException ioe) {
+					ioe.printStackTrace();
+				} finally {
+					try {
+						if (inStream != null) inStream.close();
+					} catch (IOException ioe) {
+						// nothing to see here
+					}	//	end catch
+				}	//	end finally
+				page = "";
+	    	}	//	end for
+	    	
+	    }	//	end for
+	}	//	end main
+}	//	end class
